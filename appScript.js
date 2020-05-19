ac5e4aeb309caadaef4341fd6370e47d97d97485
GenkiAri <58553174+GenkiAri@users.noreply.github.com>
2020-05-19 13:22:03 +0300

    Add files via upload

---
+++ appScript.js
@@ -0,0 +1,47 @@
+
+
+(function() {
+    var httpRequest;
+    document.getElementById('ajaxButton').addEventListener('click', makeRequest);
+
+    function makeRequest() {
+        var itemIdNumber
+        itemIdNumber = parseInt(document.getElementById('itemNumber').value);
+
+        httpRequest = new XMLHttpRequest();
+
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open('GET', 'https://api.guildwars2.com/v2/items/' + itemIdNumber);
+        httpRequest.send('');
+        
+
+    }
+    function alertContents() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            itemInfo = httpRequest.response;
+            var toUser = JSON.parse(itemInfo);
+
+            var divUser = document.createElement("div");
+            divUser.setAttribute("class","itemData");
+            divUser.innerHTML = ("Name:" + "<br>" + toUser.name + "<br>" + "Rarity:" + "<br>" + toUser.rarity + "<br>" + "Vendor value:" + "<br>" + toUser.vendor_value)
+
+            var elem = document.getElementById("result");
+            elem.parentNode.insertBefore(divUser, elem.previousSibling);
+    
+
+            // console.log(toUser)
+
+            if (httpRequest.status === 200) {
+             }  else {
+                    alert('There was a problem with a request');
+                }
+            }
+        }
+    }
+)();
+
+
