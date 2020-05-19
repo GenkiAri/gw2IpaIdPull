372c1e4e1c12d79cae37c278e764868ca97381cd
GenkiAri <58553174+GenkiAri@users.noreply.github.com>
2020-05-19 19:44:26 +0300

    Add files via upload

--- appScript.js
+++ appScript.js
@@ -1,50 +1,67 @@
 
 
-(function() {
+(function () {
     var httpRequest;
     document.getElementById('ajaxButton').addEventListener('click', makeRequest);
 
     function makeRequest() {
+        //User input item ID number
         var itemIdNumber
         itemIdNumber = parseInt(document.getElementById('itemNumber').value);
 
+        //Request to a GW2 API
         httpRequest = new XMLHttpRequest();
 
         if (!httpRequest) {
             alert('Giving up :( Cannot create an XMLHTTP instance');
             return false;
         }
-        httpRequest.onreadystatechange = alertContents;
+        httpRequest.onreadystatechange = dataProcessing;
         httpRequest.open('GET', 'https://api.guildwars2.com/v2/items/' + itemIdNumber);
         httpRequest.send('');
-        
 
     }
-    function alertContents() {
+
+
+    function dataProcessing() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            //JSON to Object conversion
             itemInfo = httpRequest.response;
             var toUser = JSON.parse(itemInfo);
 
-            var divUser = document.createElement("div");
-            divUser.setAttribute("class","itemData");
-            
-            var itemPrice = (Math.floor(toUser.vendor_value / 10000)  + " gold " +  (Math.floor(toUser.vendor_value / 100) - (Math.floor(toUser.vendor_value / 10000))*100) + " silver " + (toUser.vendor_value - (Math.floor(toUser.vendor_value / 100) * 100)  + " copper "));
+            extractDataFromObject();
 
-            divUser.innerHTML = ("Name:" + "<br>" + toUser.name + "<br>" + "Rarity:" + "<br>" + toUser.rarity + "<br>" + "Vendor value:" + "<br>" + itemPrice)
+            function extractDataFromObject() {
 
-            var elem = document.getElementById("result");
-            elem.parentNode.insertBefore(divUser, elem.previousSibling);
-    
+                calculatingItemPrice()
 
-            // console.log(toUser)
+                function calculatingItemPrice() {
+                    //Math for calculating gold, silver, copper.
+                    var itemPrice = (Math.floor(toUser.vendor_value / 10000) + " gold " + (Math.floor(toUser.vendor_value / 100) - (Math.floor(toUser.vendor_value / 10000)) * 100) + " silver " + (toUser.vendor_value - (Math.floor(toUser.vendor_value / 100) * 100) + " copper "));
+
+                    postingShowingDataToUser()
+
+                    function postingShowingDataToUser() {
+                        //Creating a div element for user to see
+                        var divUser = document.createElement("div");
+                        divUser.setAttribute("class", "itemData");
+                        divUser.innerHTML = ("Name:" + "<br>" + toUser.name + "<br>" + "Rarity:" + "<br>" + toUser.rarity + "<br>" + "Vendor value:" + "<br>" + itemPrice)
+
+                        var elem = document.getElementById("result");
+                        elem.parentNode.insertBefore(divUser, elem.previousSibling);
+                    }
+                }
+            }
+
 
             if (httpRequest.status === 200) {
-             }  else {
-                    alert('There was a problem with a request');
-                }
+            } else {
+                alert('There was a problem with a request');
             }
         }
     }
+}
 )();
+
 
 
